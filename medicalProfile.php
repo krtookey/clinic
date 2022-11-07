838a1c94b4e2f4482558447aae1dcecec316d382
eanittler <95441397+eanittler@users.noreply.github.com>
2022-11-07 17:48:16 -0500

    Medical Profile Form
    Php implementation of the Medical Profile Form.
    Change of height datatype in the MedicalProfile table from a float to a string.

--- medicalProfile.php
+++ medicalProfile.php
@@ -5,12 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medical Profile</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
     <?php
         //Variables stored in POST buffer.
-        $patient_id = $_POST['patient_id'] ?? "0";
+        $patient_id = $POST['patient_id'] ?? '';
         $preferred = $_POST['preferred'] ?? '';
         $dobirth = $_POST['dob'] ?? '';
         $age = $_POST['age'] ?? '';
@@ -18,8 +21,10 @@
         $lname = $_POST['lastname'] ?? '';
         $sex = $_POST['sex'] ?? '';
         $gender = $_POST['gender'] ?? '';
+        //$appointment = $_POST['appointment'] ?? '';
+        $appointment = 3; //for testing
 
-        //Populate Header
+        //Populate Header.
         echo "  <header>
                 <p>$preferred</p>
                 <p>$dobirth</p>
@@ -30,39 +35,169 @@
                 <p>$gender</p>
                 </header>";
     ?>
+
+    <?php
+
+        //Connect to database.
+
+        $dbServerName = "localhost"; 
+        $dbUsername = "root"; 
+        $dbPassword = ""; 
+        $dbName = "Clinic";
+                    
+        $db = new mysqli($dbServerName, $dbUsername, $dbPassword,$dbName);
+                    
+        if ($db -> connect_errno > 0){
+            echo "<p>Error: could not connect to database. <br> </p>";
+            echo "<pre> Error Number: " .$db -> errno. "\n";
+            echo "Error: "  .$db -> error. "\n <pre><br>\n";
+            exit;
+        }
+
+        //Medical Profile.
+
+        if(!isset($_POST['profileSave']) || $_POST['profileSave'] != 'Save'){
+            $_POST['profileSave'] = '';
+        }
+        if(isset($_POST['mpweight']) && $_POST['mpweight'] != ''){
+            $weight = $_POST['mpweight'];
+        }        
+        if(isset($_POST['mpheight']) && $_POST['mpheight'] != ''){
+            $height = $_POST['mpheight'];
+        }
+        if(isset($_POST['mpbmi']) && $_POST['mpbmi'] != ''){
+            $bmi = $_POST['mpbmi'];
+        }
+        if(isset($_POST['mpbloodp']) && $_POST['mpbloodp'] != ''){
+            $bloodp = $_POST['mpbloodp'];
+        }
+        if(isset($_POST['mppulse']) && $_POST['mppulse'] != ''){
+            $pulse = $_POST['mppulse'];
+        }
+        if(isset($_POST['mppulseox']) && $_POST['mppulseox'] != ''){
+            $pulseox = $_POST['mppulseox'];
+        }
+
+        //See if medical profile form has been submitted.
+        if(isset($_POST['profileSave']) || $_POST['profileSave'] == 'Save'){
+
+            $weight = $_POST['mpweight'] ?? '';
+            $height = $_POST['mpheight'] ?? '';
+            $bmi = $_POST['mpbmi'] ?? '';
+            $bloodp = $_POST['mpbloodp'] ?? '';
+            $pulse = $_POST['mppulse'] ?? '';
+            $pulseox = $_POST['mppulseox'] ?? '';
+            $medProfileID = $_POST['medProfileID'] ?? 0;
+            $num = 0;   //Counts the return to see if the appointment_id has already been asigned a Medical Profile.
+                
+            //See if a Medical Profile already exists for a given appointment 
+            $qstr = "SELECT appointment_id FROM MedicalProfile WHERE appointment_id = $appointment ";
+            $qselect = $db->prepare($qstr);
+            $qselect->execute();
+            $qselect->bind_result($appoint_id);
+            $qselect->store_result();
+                
+            while($qselect->fetch()){
+                $num = $num + 1;
+            }
+                                
+            $qselect->free_result();
+
+            //Insert Medical Profile data, for a new appointment.
+            if($num === 0){
+                $qstr = "INSERT INTO MedicalProfile (bmi, p_weight, height, blood_pressure, pulse, pulse_ox, appointment_id) VALUES (?, ?, ?, ?, ?, ?, $appointment ) ";
+                $qinsert = $db->prepare($qstr);
+                if(!$qinsert){
+                    echo "<p>Error: could not execute query. <br> </p>";
+                    echo "<pre> Error Number: " .$db -> errno. "\n";
+                    echo "Error: "  .$db -> error. "\n <pre><br>\n";
+                    exit;
+                }
+                $qinsert->bind_param("dddsii", $bmi, $weight, $height, $bloodp, $pulse, $pulseox);
+                $qinsert->execute();
+                $qinsert->store_result();
+                $qinsert->free_result();
+
+                //Get new med_profile_id.
+                $qstr = "SELECT med_profile_id FROM MedicalProfile WHERE appointment_id = $appointment ";
+                $qselect = $db->prepare($qstr);
+                $qselect->execute();
+                $qselect->bind_result($med_profile_id);
+                $qselect->store_result();
+
+                while($qselect->fetch()){
+                    //Debug: echo "New ID: $med_profile_id";
+                    $_POST['medProfileID'] = $med_profile_id;
+                }
+                
+                $qselect->free_result();
+
+                //Debug: echo "<pre>"; print_r($_POST); echo "</pre>";
+                
+                //Updating an existing Medical Profile data.
+                } elseif ($num === 1) {
+                    $qstr = "UPDATE MedicalProfile 
+                             SET bmi = ?, p_weight = ?, height = ?, blood_pressure = ?, pulse = ?, pulse_ox = ?, appointment_id = $appointment 
+                             WHERE appointment_id = $appointment ";
+                    $qupdate = $db->prepare($qstr);
+                    if(!$qupdate){
+                        echo "<p>Error: could not execute query. <br> </p>";
+                        echo "<pre> Error Number: " .$db -> errno. "\n";
+                        echo "Error: "  .$db -> error. "\n <pre><br>\n";
+                        exit;
+                    }
+                    $qupdate->bind_param("dddsii", $bmi, $weight, $height, $bloodp, $pulse, $pulseox);
+                    $qupdate->execute();
+                    $qupdate->store_result();
+                    $qupdate->free_result();
+                }
+        } //End of Medical Profile query statement.
+            
+    ?>
     <div class="placeholder"></div>
     <div class="whiteCard">
         <h2>Medical Profile</h2> <br>
-        <form id="profileForm">
+        <form method="post" action="./medicalProfile.php" id="profileForm">
             <div id="profileGrid">
                 <div class="profileItem">
                     <label>Weight:</label>
-                    <input type="text">
+                    <input type="text" name="mpweight" id="mpwight" value=<?php echo $weight; ?>>
                 </div>
                 <div class="profileItem">
                     <label>Height:</label>
-                    <input type="text">
+                    <input type="text" name="mpheight" id="mpheight" value=<?php echo $height; ?>>
                 </div>
                 <div class="profileItem">
                     <label>BMI:</label>
-                    <input type="text">
+                    <input type="text" name="mpbmi" id="mpbmi" value=<?php echo $bmi; ?>>
                 </div>
                 <div class="profileItem">
                     <label>Blood Pressure:</label> 
-                    <input type="text">
+                    <input type="text" name="mpbloodp" id="mpbloodp" value=<?php echo $bloodp; ?>>
                 </div>
                 <div class="profileItem">
                     <label>Pulse:</label>
-                    <input type="text">
+                    <input type="text" name="mppulse" id="mppulse" value=<?php echo $pulse; ?>>
                 </div>
                 <div class="profileItem">
                     <label>Pulse OX:</label>
-                    <input type="text">
+                    <input type="text" name="mppulseox" id="mppulseox" value=<?php echo $pulseox; ?>>
                 </div>
             </div>
             <div class="saveButton">
-                <input type="submit" value="Save" id="ProfileSave">
+                <input type="submit" value="Save" name="profileSave" id="profileSave" >
             </div>
+                <input type="hidden" name="medProfileID">
+                <?php
+                    echo "<input type='hidden' name='patient_id' value='$patient_id'>";
+                    echo "<input type='hidden' name='preferred' value='$preferred'>";
+                    echo "<input type='hidden' name='dob' value='$dobirth'>";
+                    echo "<input type='hidden' name='age' value='$age'>";
+                    echo "<input type='hidden' name='firstname' value='$fname'>";
+                    echo "<input type='hidden' name='lastname' value='$lname'>";
+                    echo "<input type='hidden' name='sex' value='$sex'>";
+                    echo "<input type='hidden' name='gender' value='$gender'>";
+                ?>
         </form>
     </div>
     <div class="whiteCard">
@@ -606,7 +741,7 @@
             <div></div>
             <div></div>
             <div class="saveButton">
-                <input type="submit" value="Save" id="ProfileSave">
+                <input type="submit" value="Save" id="rosSave">
             </div>
         </form>
     </div>
@@ -616,5 +751,41 @@
             <a href="./patient.php">Patient Note</a>
         </div>
     </footer>
+    <?php
+        $db->close();
+    ?>
+    <script type="text/javascript">
+        //Temporary appointment_id for testing.
+        //let appointment = 4;
+
+        //Ajax call for saving the Medical Profile form. Not Working.
+        function medProfileSave(){
+            let weight = document.getElementById('mpweight').value;
+            let height = document.getElementById('mpheight').value;
+            let bmi = documnet.getElementById('mpbmi').value;
+            let bloodp = document.getElementById('mpbloodp').value;
+            let pulse = document.getElementById('mppulse').value;
+            let pulseox = document.getElementById('mppulseox').value;
+
+            $.ajax({
+                type: 'post',
+                url:  './medProfileSave.php',
+                data: {
+                    'appointment':appointment,
+                    'weight' :weight,
+                    'height' :height,
+                    'bmi' :bmi,
+                    'bloodPressure' :bloodp,
+                    'pulse' :pulse,
+                    'pulseox' :pulseox
+                },
+                cache: false,
+                success: function(msg) {
+                    alert('Medical Profile Saved');
+                }               
+            });
+            return false;
+        }
+    </script>
 </body>
 </html>
