c8b53fab5e6a23e27ab85cb218410679cf6deb1d
Duncan <duncanmc2000@gmail.com>
2022-11-13 19:08:37 -0500

    Note page now retrieves patient data, needs format work.

--- noteHistory.php
+++ noteHistory.php
@@ -14,18 +14,23 @@
   <header id='patientHeader'>
       <?php
       //SQL
-      $sql = "SELECT Patient.first_name, Patient.last_name, Patient.DOB, Patient.sex, Patient.preferred
+      $query = "SELECT Patient.first_name, Patient.last_name, Patient.DOB, Patient.sex, Patient.preferred
       FROM Patient
       WHERE Patient.patient_id = ?";
+
       //Prepare statment
-      $stmt = $conn->prepare($sql);
+      $stmt = $conn->prepare($query);
+
       //Bind ? with the POST variable from the prvious page
       $patient_id = $POST['patient_id'] ?? 1; //TODO remove after testing
       $stmt->bind_param("i", $patient_id);
+
       //Execute and get resutls from database
       $stmt->execute();
+
       $result = $stmt->get_result();
       $row = $result->fetch_row();
+
       $patient_firstName = $row[0];
       $patient_lastName = $row[1];
       $patient_DOB = date_create($row[2]);
@@ -34,78 +39,54 @@
       $patientYears = $now->diff($patient_DOB);
       $patient_sex = $row[3];
       $patient_preferred = $row[4];
+
+
       echo "<p>$patient_firstName</p>
       <p>$patient_lastName</p>
       <p>pref: $patient_preferred</p>
       <p>DOB: $patient_DOBFormatted</p>
       <p>$patientYears->y" . "y</p>
       <p>$patient_sex</p>";
+
       ?>
   </header>
 
-  <div class="placeholder"></div>
   <div id="noteHistory">
       <h2>Notes</h2> <br>
 
 <form method='post' action='NoteHistory.php'>
 <?php
-//Wondering if I need this here or should I only connect when needed.
-$db = new mysqli('localhost', 'root', '', 'Clinic');
-if ($db->connect_errno > 0){
-  echo "<p>Error: Could not connect to database.<br></p>";
-  echo "<pre>\nErrno: " . $db->errno . "\n";
-  echo "Error: " . $db->error . "\n</pre><br>\n";
+//grabbing row of patient data
+$query = "SELECT *
+FROM Note
+WHERE Note.patient_id = ?";
+$stmt = $conn->prepare($query);
 
-   exit;
-}
-//Need patient Id, appoint_id, cc, hist_illness, ros_id, med_profile_id, social_hist, med_hist, psych_hist, assessment, plan, laborder_id, lab_destid, demographics, comments
-//To-do: make table via PHP
-//Grab patient data
+$patient_id = $POST['patient_id'] ?? 1; //TODO remove after testing
+$stmt->bind_param("i", $patient_id);
+
+//Execute and get results from database
+$stmt->execute();
+
+$stmt->store_result();
+$stmt->bind_result($note_id, $patient_id, $appoint_id, $cc, $hist_illness, $ros_id, $med_profile_id, $social_hist, $med_hist, $psych_hist, $assessment, $plan, $laborder_id, $lab_destid, $demographics, $comments);
+
+//be able to swap notes
+//show only certain parts of a note
 //order it by most recent
-//move the header to top of page
 //figure out how to go and edit the selected note
-//
+
+//Attempts on trying to get multiple column data
+while ($stmt->fetch()){
+  echo "<table border='1'>";
+
+//debugging for right now, need to remove ids
+  echo "<tr><td>$note_id</td> <td>$patient_id</td><td>$appoint_id</td><td>$cc</td><td>$hist_illness</td><td>$ros_id</td><td>$med_profile_id</td><td>$social_hist</td><td>$med_hist</td><td>$psych_hist</td><td>$assessment</td><td>$plan</td><td>$laborder_id</td><td>$lab_destid</td><td>$demographics</td><td>$comments</td> </tr>";
+  echo "<table></br>";
+
+}
+
 ?>
-
-      <table id ="noteTable">
-        <tr>
-          <th>Date: </th>
-          <th>Date: </th>
-          <th>Date: </th>
-          <th>Date: </th>
-        </tr>
-        <tr>
-          <td>Data from patients notes</td>
-          <td>Data from patients notes</td>
-          <td>Data from patients notes</td>
-          <td>Data from patients notes</td>
-        </tr>
-        <tr>
-          <th>Date: </th>
-          <th>Date: </th>
-          <th>Date: </th>
-          <th>Date: </th>
-        </tr>
-        <tr>
-          <td>Data from patients notes</td>
-          <td>Data from patients notes</td>
-          <td>Data from patients notes</td>
-          <td>Data from patients notes</td>
-        </tr>
-        <tr>
-          <th>Date: </th>
-          <th>Date: </th>
-          <th>Date: </th>
-          <th>Date: </th>
-        </tr>
-        <tr>
-          <td>Data from patients notes</td>
-          <td>Data from patients notes</td>
-          <td>Data from patients notes</td>
-          <td>Data from patients notes</td>
-        </tr>
-      </table>
-
 
   </div>
   <footer>
