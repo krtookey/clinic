9d7eeae0898a09ec870bdc7727e33280ca7fb7af
vtwarrior25 <vtwarrior25@gmail.com>
2022-11-18 08:40:57 -0500

    Added some formatting to add pharmacy and labdest forms

--- addpharmacyform.php
+++ addpharmacyform.php
@@ -5,20 +5,25 @@
     <input type="text" id="pharmacy_name" name="pharmacy_name" required>
     <fieldset id="pharmacy_address">
         <legend>Address</legend>
-        <label for="pharmacy_street">Street:</label>
-        <input type="text" id="pharmacy_street" name="pharmacy_street" required>
-        <label for="pharmacy_city">City:</label>
-        <input type="text" id="pharmacy_city" name="pharmacy_city" required>
-        <label for="pharmacy_state">State:</label>
-        <input type="text" id="pharmacy_state" name="pharmacy_state" required>
-        <label for="pharmacy_zip">ZIP:</label>
-        <input type="number" id="pharmacy_zip" name="pharmacy_zip" required>
-    </fieldset>    
-    <label for="pharmacy_phone">Phone #:</label>
-    <input type="tel" id="pharmacy_phone" name="pharmacy_phone" required>
-    <br>
-    <label for="pharmacy_email">Email:</label>
-    <input type="email" id="pharmacy_email" name="pharmacy_email">
-    <input type="submit" value="Submit"">
+        <div class="rowOfInputs pharmacyInput">
+            <label for="pharmacy_street">Street:</label>
+            <input type="text" id="pharmacy_street" name="pharmacy_street" required>
+            <label for="pharmacy_city">City:</label>
+            <input type="text" id="pharmacy_city" name="pharmacy_city" required>
+        </div>
+        <div class="rowOfInputs pharmacyInput">
+            <label for="pharmacy_state">State:</label>
+            <input type="text" id="pharmacy_state" name="pharmacy_state" required>
+            <label for="pharmacy_zip">ZIP:</label>
+            <input type="number" id="pharmacy_zip" name="pharmacy_zip" required>
+        </div>
+    </fieldset>
+    <div class="rowOfInputs pharmacyInput">
+        <label for="pharmacy_phone">Phone #:</label>
+        <input type="tel" id="pharmacy_phone" name="pharmacy_phone" required>
+        <label for="pharmacy_email">Email:</label>
+        <input type="email" id="pharmacy_email" name="pharmacy_email">
+        <input type="submit" value="Submit"">
+    </div>    
     <iframe name="addnewpharmacyframe" class="results_iframe"></iframe>
 </form>
