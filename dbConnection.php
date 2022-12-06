049da115ecda4f5097cca173e90c9639bbf2b374
vtwarrior25 <vtwarrior25@gmail.com>
2022-12-06 12:16:15 -0500

    Added check to ensure that SQL user is able to select patient

--- dbConnection.php
+++ dbConnection.php
@@ -12,4 +12,13 @@
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     }
+    // Make sure that user has permissions that are required for the rest of the system to work
+    $sql = "SELECT Patient.first_name, Patient.last_name, Patient.DOB, Patient.sex, Patient.preferred
+            FROM Patient
+            WHERE Patient.patient_id = 1";
+    try {    
+        $result = $conn->query($sql);   
+    } catch (Exception $e) {
+        echo 'Caught exception: ', $e->getMessage(), "\n";
+    }
 ?>
