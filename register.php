d8e5079b1329f7f45186121f88539ebc0544f9da
Master530 <39843411+Master530@users.noreply.github.com>
2022-12-07 10:37:54 -0500

    login/register
    both pages are functional (depending on machine) designe has been added, need logo's and images still. Significant portion of time went into database troubleshooting.

--- register.php
+++ register.php
@@ -1,13 +1,13 @@
 <?php 
 
-	// Allow the config
-	define('__CONFIG__', true);
-	// Require the config
-	require_once "inc/config.php"; 
-	Page::ForceDashboard();
+  // Allow the config
+  define('__CONFIG__', true);
+  // Require the config
+  include "inc/config.php"; 
+ 
 
+  Page::ForceDashboard();
 ?>
-	
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,42 +21,75 @@
     <title>Page Title</title>
 
     <base href="/" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
   </head>
 
   <body>
 
-  	<div class="uk-section uk-container">
-  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-			<form class="uk-form-stacked js-register">
-				
-				<h2>Register</h2>
+    <section class="vh-100" style="background-color: #9A616D;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-10">
+        <div class="card" style="border-radius: 1rem;">
+          <div class="row g-0">
+            <div class="col-md-6 col-lg-5 d-none d-md-block">
+              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+            </div>
+              <iframe name="registerdisplay" class="results_iframe"></iframe>
+            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="card-body p-4 p-lg-5 text-black">
 
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Email</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
-			        </div>
-			    </div>
+                <form action="register_script.php" method="post" id="registerform">
 
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Passphrase</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your passphrase">
-			        </div>
-			    </div>
+                  <div class="d-flex align-items-center mb-3 pb-1">
+                    <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                    <span class="h1 fw-bold mb-0">Logo</span>
+                  </div>
 
-			    <div class="uk-margin uk-alert uk-alert-danger js-error" style='display: none;'></div>
+                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Register your new account</h5>
 
-			    <div class="uk-margin">
-			        <button class="uk-button uk-button-default" type="submit">Register</button>
-			    </div>
-
-			</form>
-  		</div>
-  	</div>
-
-  	<?php require_once "inc/footer.php"; ?> 
+                  <div class="form-outline mb-4">
+                    <input type="text" name="fname" id="form2Example27" class="form-control form-control-lg" required/>
+                    <label class="form-label" for="form2Example27">First Name</label>
+                  </div>
+                  <div class="form-outline mb-4">
+                    <input type="text" name="lname" id="form2Example27" class="form-control form-control-lg" required/>
+                    <label class="form-label" for="form2Example27">Last Name</label>
+                  </div>
+                  <div class="form-outline mb-4">
+                    <input type="email" name="email" id="form2Example17" class="form-control form-control-lg" required/>
+                    <label class="form-label" for="form2Example17">Email Address</label>
+                  </div>
+                  <div class="form-outline mb-4">
+                    <input type="text" name="uname" id="form2Example27" class="form-control form-control-lg" required/>
+                    <label class="form-label" for="form2Example27">Username</label>
+                  </div>
+                  <div class="form-outline mb-4">
+                    <input type="phone" name="phone" id="form2Example27" class="form-control form-control-lg" required/>
+                    <label class="form-label" for="form2Example27">Phone Number</label>
+                  </div>
+                  <div class="form-outline mb-4">
+                    <input type="text" name="title" id="form2Example27" class="form-control form-control-lg" required/>
+                    <label class="form-label" for="form2Example27">Job Title</label>
+                  </div>
+                  <div class="form-outline mb-4">
+                    <input type="password" name="password" id="form2Example27" class="form-control form-control-lg" required/>
+                    <label class="form-label" for="form2Example27">Password</label>
+                  </div>
+                  <div class="pt-1 mb-4">
+                    <button class="btn btn-dark btn-lg btn-block" type="submit">Register</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    <?php require_once "inc/footer.php"; ?> 
   </body>
-
 </html>
